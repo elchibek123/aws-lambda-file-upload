be91077f3717d6bd634fb8115483c98a077db227
elchibek123 <elchibek.kamalov@gmail.com>
2024-05-26 11:10:52 -0400

    updated api

--- upload_binary.py
+++ upload_binary.py
@@ -1,12 +1,37 @@
 import json
 import base64
 import boto3
-
+from botocore.exceptions import ClientError
 
 def lambda_handler(event, context):
     s3 = boto3.client("s3")
-    get_file_content = event["content"]
-    decode_content = base64.b64decode(get_file_content)
-    s3_upload = s3.put_object(Bucket="bucket-name", Key="file.pdf", Body=decode_content)
+    bucket_name = "private-sandbox-us-east-1-s3-bucket-001"
+    object_key = "file.pdf"
 
-    return {"statusCode": 200, "body": json.dumps("Thanks for using!")}
+    try:
+        get_file_content = event["content"]
+        decode_content = base64.b64decode(get_file_content)
+        s3.put_object(Bucket=bucket_name, Key=object_key, Body=decode_content)
+        
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Thanks for using!")
+        }
+
+    except KeyError:
+        return {
+            "statusCode": 400,
+            "body": json.dumps("The request did not contain 'content' key.")
+        }
+
+    except ClientError as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"An error occurred: {e}")
+        }
+
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(f"An unexpected error occurred: {e}")
+        }
